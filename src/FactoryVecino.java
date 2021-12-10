689e4014f52529d476add7e965c9b6ed40feb9b7
YeseniaCueva <u20211a653@upc.edu.pe>
2021-12-09 20:41:59 -0500

    Factoría de vecino

---
+++ src/FactoryVecino.java
@@ -0,0 +1,25 @@
+public class FactoryVecino {
+    //tipo = 1 : retornar ClubEcologia | 2 : retornar Adulto Mayor
+    public static Vecino obtenerVecino(int tipo,String... data){
+        switch (tipo){
+            case 1 : return new ClubEcologia(
+                    data[0],
+                    data[1],
+                    data[2],
+                    data[3],
+                    Integer.parseInt(data[4]),
+                    data[5]
+            );
+            case 2 : return new AdultoMayor(
+                    data[0],
+                    data[1],
+                    data[2],
+                    data[3],
+                    Integer.parseInt(data[4]),
+                    data[5]
+            );
+            default: return null;
+        }
+
+    }
+}
